df7329dac3730c93e217ed1d83fcbbda205797e2
majpark21 <marqui.thomas@yahoo.fr>
2017-07-27 14:23:00 +0200

    simplified dist_mean

--- dist_mean.R
+++ dist_mean.R
@@ -12,8 +12,6 @@ dist_mean <- function(data, condition, tcol, measure, label, return.mean = F){
   # label: column name with label of individual objects in each condition (cell label)
   
   require(data.table)
-  setkeyv(data, condition)
-  
   # 1) Get mean trajectory for each condition
   means <- data[, .(Average = mean(get(measure))), by = c(condition, tcol)]
   dat <- merge(data[, c(condition, label, tcol, measure), with = F], means, by = c(condition, tcol))
@@ -21,7 +19,6 @@ dist_mean <- function(data, condition, tcol, measure, label, return.mean = F){
   # 2) Euclidean distance between each trajectory and the mean one
   distances <- dat[, .(sq_dist = (get(measure) - Average)^2) , by = c(condition, label)]
   euclid <- distances[, .(euclid_to_mean = sqrt(sum(sq_dist))), by = c(condition, label)]
-
   
   if(return.mean){
     return(list(means = means, euclid = euclid))
@@ -29,6 +26,7 @@ dist_mean <- function(data, condition, tcol, measure, label, return.mean = F){
     return(euclid)
   }
 }
+
 
 #####
 
@@ -38,9 +36,9 @@ setkey(Cora, Image_Metadata_Site)
 #means <- Cora[, .(mean.traj = mean(Ratio)), by = .(Image_Metadata_Site, RealTime),]
 
 
-euclids <- dist_mean(Cora, "Image_Metadata_Site", "RealTime", "Ratio", "objNuc_TrackObjects_Label", return.mean = T)
+euclids <- dist_mean(Cora, "Image_Metadata_Site", "RealTime", "Ratio", "objNuc_TrackObjects_Label", return.mean = F)
 #setkey(euclids, Image_Metadata_Site)
-#euclids[, .(Mean = mean(euclid_to_mean), Variance = var(euclid_to_mean), Min = min(euclid_to_mean), Max = max(euclid_to_mean)), Image_Metadata_Site]
+euclids[, .(Mean = mean(euclid_to_mean), Variance = var(euclid_to_mean), Min = min(euclid_to_mean), Max = max(euclid_to_mean)), Image_Metadata_Site]
 
 
 p <- ggplot(euclids$euclid, aes(x=Image_Metadata_Site, y=euclid_to_mean)) + geom_boxplot(aes(group=Image_Metadata_Site)) + scale_y_continuous(limits = c(0,4.5))
