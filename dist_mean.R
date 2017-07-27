5b029e1466b32b19c82b19da82ad490f66064d2c
majpark21 <marqui.thomas@yahoo.fr>
2017-07-27 14:20:28 +0200

    simplified dist_mean

--- dist_mean.R
+++ dist_mean.R
@@ -15,16 +15,13 @@ dist_mean <- function(data, condition, tcol, measure, label, return.mean = F){
   setkeyv(data, condition)
   
   # 1) Get mean trajectory for each condition
-  means <- data[, .(Average = mean(get(measure))), by = .(get(condition), get(tcol))]
-  colnames(means)[1:2] <- c(condition, tcol)
+  means <- data[, .(Average = mean(get(measure))), by = c(condition, tcol)]
   dat <- merge(data[, c(condition, label, tcol, measure), with = F], means, by = c(condition, tcol))
   
   # 2) Euclidean distance between each trajectory and the mean one
-  distances <- dat[, .(sq_dist = (get(measure) - Average)^2) , by = .(get(condition), get(label))]
-  colnames(distances)[1:2] <- c(condition, label)
-  
-  euclid <- distances[, sqrt(sum(sq_dist)), by = .(get(condition), get(label))]
-  colnames(euclid) <- c(condition, label, "euclid_to_mean")
+  distances <- dat[, .(sq_dist = (get(measure) - Average)^2) , by = c(condition, label)]
+  euclid <- distances[, .(euclid_to_mean = sqrt(sum(sq_dist))), by = c(condition, label)]
+
   
   if(return.mean){
     return(list(means = means, euclid = euclid))
@@ -33,7 +30,7 @@ dist_mean <- function(data, condition, tcol, measure, label, return.mean = F){
   }
 }
 
-
+#####
 
 Cora <- fread("C:/Users/pixel/Dropbox/Marc-Antoine/data/set1-Coralie/tCoursesSelected.csv")
 Cora[, Ratio := objCyto_Intensity_MeanIntensity_imErkCorrOrig / objNuc_Intensity_MeanIntensity_imErkCorrOrig]
@@ -41,9 +38,9 @@ setkey(Cora, Image_Metadata_Site)
 #means <- Cora[, .(mean.traj = mean(Ratio)), by = .(Image_Metadata_Site, RealTime),]
 
 
-euclids <- dist_mean(Cora, "Image_Metadata_Site", "RealTime", "Ratio", "objNuc_TrackObjects_Label", return.mean = F)
-setkey(euclids, Image_Metadata_Site)
-euclids[, .(Mean = mean(euclid_to_mean), Variance = var(euclid_to_mean), Min = min(euclid_to_mean), Max = max(euclid_to_mean)), Image_Metadata_Site]
+euclids <- dist_mean(Cora, "Image_Metadata_Site", "RealTime", "Ratio", "objNuc_TrackObjects_Label", return.mean = T)
+#setkey(euclids, Image_Metadata_Site)
+#euclids[, .(Mean = mean(euclid_to_mean), Variance = var(euclid_to_mean), Min = min(euclid_to_mean), Max = max(euclid_to_mean)), Image_Metadata_Site]
 
 
 p <- ggplot(euclids$euclid, aes(x=Image_Metadata_Site, y=euclid_to_mean)) + geom_boxplot(aes(group=Image_Metadata_Site)) + scale_y_continuous(limits = c(0,4.5))
