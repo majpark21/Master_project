13df3a481b89ae6733bc842dae8644c505583b06
majpark21 <marqui.thomas@yahoo.fr>
2017-07-28 12:38:43 +0200

    simulations and plot

--- sim_data.R
+++ sim_data.R
@@ -15,6 +15,8 @@ sim_phase_shifted <- function(n, noise, freq = 1 ,len = 100, return.wide = F){
 
   # Replace each shifted time by it sine function
   sins <- sin(time_matrix)
+  
+  # Go to data.table
   sins <- as.data.table(sins)
   sins <- cbind(seq(0, len-1, freq), sins)
   colnames(sins)[1] <- "Time"
@@ -28,5 +30,25 @@ sim_phase_shifted <- function(n, noise, freq = 1 ,len = 100, return.wide = F){
 }
 
 
-temp <- sim_phase_shifted(5, 1, 1, 50, return.wide = T)
+plot_sim <- function(data, alpha = 0.2){
+  require(ggplot2)
+  p <- ggplot(data, aes(x = Time, y = value)) + geom_line(aes(group = variable), alpha = alpha)
+  p <- p + stat_summary(fun.y=mean, geom="line", colour = "blue", size = 1.5)
+  p
+}
+plot_sim(sim2)
 
+
+# TODO: Implement condition as noise, simulate for a vector of noise?
+sim <- sim_phase_shifted(n = 50, noise = 0.1)
+sim$condition <- 1
+a <- dist_mean(sim, "condition", "Time", "value", "variable")
+
+
+sim2 <- sim_phase_shifted(n = 50, noise = 1)
+sim2$condition <- 1
+b <- dist_mean(sim2, "condition", "Time", "value", "variable")
+
+par(mfrow=c(1,2))
+boxplot(a$euclid_to_mean, ylim = c(0,10), main="noise 1")
+boxplot(b$euclid_to_mean, ylim = c(0,10), main="noise X")
